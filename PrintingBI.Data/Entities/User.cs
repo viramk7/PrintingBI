b1043cd6ebc43138c26ff417b2acbdac14178df1
Viram Keshwala <viramk7@gmail.com>
2019-04-30 21:08:03 +0530

    Initial commit

---
+++ PrintingBI.Data/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace PrintingBI.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PrintingBI.Data/PrintingBIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrintingBI.Data.Entities;
+
+namespace PrintingBI.Data
+{
+    public class PrintingBIDbContext : DbContext
+    {
+        public PrintingBIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
